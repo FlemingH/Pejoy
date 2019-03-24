dbaaad0ba2ce4f1d9d61de4adfda499235205e85
Fleming <16278383@qq.com>
2019-03-25 00:45:54 +0800

    impl check login info ajax

--- web/resource/vue_js/login/pejoy_login_panel.js
+++ web/resource/vue_js/login/pejoy_login_panel.js
@@ -6,49 +6,58 @@ Vue.component('PejoyLoginPanel', {
         return{
             isUsernameNull: false,
             isPasswordNull: false,
-            isRUsernameNull: false,
-            isRPasswordNull: false,
             errorTitle: "",
             errorMessage: "",
             curForm: 0,
             curUsername: "",
-            curPassword: ""
+            curPassword: "",
+            loadingState: 0,
         }
     },
     watch: {
 
     },
     methods: {
+        init() {
+            this.reset();
+        },
+        reset() {
+            this.hideErrorMessage();
+            this.curUsername = "";
+            this.curPassword = "";
+            this.loadingState = 0;
+            this.isUsernameNull = false;
+            this.isPasswordNull = false;
+            this.isRUsernameNull = false;
+            this.isRPasswordNull = false;
+        },
         switchArea() {
+            this.reset();
             if(this.curForm == 0) {
                 this.curForm = 1;
             } else {
                 this.curForm = 0;
             }
         },
-        checkLoginInput() {
-            if(!$('#usernameInput').val()) {
+        checkInput() {
+            if(!this.curUsername) {
                 this.isUsernameNull = true;
             } else {
                 this.isUsernameNull = false;
             }
-            if(!$('#passwordInput').val()) {
+            if(!this.curPassword) {
                 this.isPasswordNull = true;
             } else {
                 this.isPasswordNull = false;
             }
         },
-        checkRegisterInput() {
-            if(!$('#usernameInputR').val()) {
-                this.isRUsernameNull = true;
-            } else {
-                this.isRUsernameNull = false;
-            }
-            if(!$('#passwordInputR').val()) {
-                this.isRPasswordNull = true;
-            } else {
-                this.isRPasswordNull = false;
-            }
+        showErrorMessage() {
+            $('.errorMsgArea').show();
+        },
+        hideErrorMessage() {
+            this.errorTitle = '';
+            this.errorMessage = '';
+            $('.errorMsgArea').hide();
         },
         registerOnClick() {
             this.switchArea();
@@ -57,18 +66,71 @@ Vue.component('PejoyLoginPanel', {
             this.switchArea();
         },
         loginButtonOnClick() {
-            this.checkLoginInput();
-            this.curUsername = $('#usernameInput').val();
-            this.curPassword = $('#passwordInput').val();
+            this.checkInput();
 
+            if(this.curUsername && this.curPassword) {
+                this.checkLoginInfo();
+            }
         },
         registerButtonOnClick() {
-            this.checkRegisterInput();
-            this.curUsername = $('#usernameInputR').val();
-            this.curPassword = $('#passwordInputR').val();
+            this.checkInput();
+        },
+        checkLoginInfo() {
+
+            var self = this;
+
+            var options = {
+                username: this.curUsername,
+                password: this.curPassword
+            }
+
+            self.loadingState = 1;
+            self.hideErrorMessage();
+
+            $.ajax({
+                url: "http://127.0.0.1:1123/checkLoginInfo",
+                type: "POST",
+                data: options
+            }).done(function (respones) {
+                self.loadingState = 0;
+
+                if("data not found" == respones.messageData) {
+
+                    self.errorTitle = "登录失败！";
+                    self.errorMessage = "用户不存在，请检查输入的信息。";
+                    self.showErrorMessage();
+                    console.log("check login info fail", respones);
+
+                } else if("db error" == respones.messageData) {
+
+                    self.errorTitle = "登录失败！";
+                    self.errorMessage = "用户信息错误，检查输入的信息。";
+                    self.showErrorMessage();
+                    console.log("check login info fail", respones);
+
+                } else if("data error" == respones.messageData) {
+
+                    self.errorTitle = "登录失败！";
+                    self.errorMessage = "用户信息错误，检查输入的信息。";
+                    self.showErrorMessage();
+                    console.log("check login info fail", respones);
+
+                } else if(!respones.messageData && respones.messageState == "success") {
+                    console.log("check login info success", respones);
+                }
+
+            }).fail(function (respones) {
+                self.loadingState = 0;
+                self.errorTitle = "登录失败！"
+                self.errorMessage = "网络错误。"
+                self.showErrorMessage();
+                console.log("check login info fail", respones);
+            });
+
         }
     },
     mounted: function() {
+        this.init();
         console.debug("NewGoMainPanel-template mounted");
     }
 });
